13701bf06c18301266e081a29eb38add4e1cec94
s0urce18 <54136752+s0urce18@users.noreply.github.com>
2022-05-02 09:23:49 +0300

    Задачи 4-7

---
+++ RU/Задачи/Задача 7.py
@@ -0,0 +1,19 @@
+people = [] # основной массив
+'''структура словаря
+{
+    "firstName": "",
+    "secondName": "",
+    "age": "",
+    "country": "",
+    "city": ""
+}
+'''
+
+def create_people(first_name, second_name, age, country, city):
+    people.append({
+        "firstName": first_name,
+        "secondName": second_name,
+        "age": age,
+        "country": country,
+        "city": city
+    })
